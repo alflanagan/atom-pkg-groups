a33ab6284a47722345768e4dd0df5970366693a0
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2017-08-04 12:08:58 -0400

    initial commit

---
+++ spec/pkg-groups-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PkgGroupsView from '../lib/pkg-groups-view';
+
+describe('PkgGroupsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
