279ffe55149f4daad431d6f8b05809959e1ad55c
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2017-09-15 07:13:23 -0400

    some fixes, complete serialize()

--- spec/pkg-groups-view-spec.js
+++ spec/pkg-groups-view-spec.js
@@ -2,10 +2,13 @@
 
 /* eslint-env jasmine */
 
-// import PkgGroupsView from '../lib/pkg-groups-view'
+import PkgGroupsView from '../lib/pkg-groups-view'
 
 describe('PkgGroupsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy')
+  it('constructs', () => {
+    let view = new PkgGroupsView()
+    expect(view instanceof PkgGroupsView).toBe(true)
+    expect(view.props).toEqual({})
+    expect(view.children).toEqual([])
   })
 })
