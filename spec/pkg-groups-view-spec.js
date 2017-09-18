b8d6d096bfb49a8a80cddb4baa89dd71d4dedb7b
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2017-09-18 07:34:18 -0400

    more constructor tests

--- spec/pkg-groups-view-spec.js
+++ spec/pkg-groups-view-spec.js
@@ -1,14 +1,22 @@
 /** @babel */
-
 /* eslint-env jasmine */
+
+import etch from 'etch'
 
 import PkgGroupsView from '../lib/pkg-groups-view'
 
 describe('PkgGroupsView', () => {
-  it('constructs', () => {
-    let view = new PkgGroupsView()
-    expect(view instanceof PkgGroupsView).toBe(true)
-    expect(view.props).toEqual({})
-    expect(view.children).toEqual([])
+  describe('constructor', () => {
+    it('does not require arguments', () => {
+      let fred = new PkgGroupsView()
+      expect(fred.children).not.toExist()
+      expect(fred.props).toEqual({})
+      // attributes added to component by etch.initialize()
+      expect(fred.virtualNode['tag']).toEqual('div')
+      expect(fred.virtualNode['props']['className']).toEqual('pkg-groups')
+      expect(fred.virtualNode['children']).toBeInstanceOf(Array)
+      expect(fred.element).toExist()
+      expect(fred.refs).toEqual({})
+    })
   })
 })
