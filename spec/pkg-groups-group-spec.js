519e9fef9b09396e08af047b7cc1c41ad9ad5dac
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2017-10-07 13:28:22 -0400

    Add tests for forEach(), clarify docs

--- spec/pkg-groups-group-spec.js
+++ spec/pkg-groups-group-spec.js
@@ -5,7 +5,7 @@ import log4js from 'log4js'
 import Immutable from 'immutable'
 import PkgGroupsGroup from '../lib/pkg-groups-group'
 
-const logger = log4js.getLogger('pkg-groups-spec')
+const logger = log4js.getLogger('pkg-groups-group-spec')
 logger.level = 'debug'
 
 describe('PkgGroupsGroup', () => {
@@ -39,5 +39,23 @@ describe('PkgGroupsGroup', () => {
     expect(data['packages']).toContain('pkg-groups')
   })
 
-  it('can call a function with each package', () => {})
+  it('can call a function with each package', () => {
+    let actual = []
+    /* actual signature is (value, key, iter) */
+    /* for a Set value === key, and iter === the Set */
+    const add = (value) => actual.push(value)
+    betty.forEach(add)
+    const expected = ['pkg-groups', 'MagicPython', 'project-manager']
+    expect(actual).toEqual(expected)
+  })
+
+  it('can pass a context to the callback', () => {
+    let actualContext = { pkgs: [] }
+    const expected = ['pkg-groups', 'MagicPython', 'project-manager']
+    /* actual signature is (value, key, iter) */
+    /* for a Set value === key, and iter === the Set */
+    const add = function (value) { this.pkgs.push(value) }
+    betty.forEach(add, actualContext)
+    expect(actualContext.pkgs).toEqual(expected)
+  })
 })
