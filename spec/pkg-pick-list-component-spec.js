0626ae9ba53f5bd925c6726d2582a9eed59b32a8
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2017-10-15 10:39:07 -0400

    Add handlers for buttons, select lists

--- spec/pkg-pick-list-component-spec.js
+++ spec/pkg-pick-list-component-spec.js
@@ -7,12 +7,14 @@ import etch from 'etch'
 import log4js from 'log4js'
 import Immutable from 'immutable'
 
-import DomDiff from '../lib/pkg-groups-dom-diff'
+import DomDiff from '../lib/pkg-groups-dom-diff' // eslint-disable-line no-unused-vars
 import PkgPickList from '../lib/pkg-pick-list-component'
-import PkgListComponent from '../lib/pkg-list-component'
+import PkgSelectList from '../lib/pkg-select-list-component'
+import {promiseAnEvent} from '../lib/pkg-groups-functions'
+import {PickListChangeEvent} from '../lib/pkg-groups-events'
 
 const logger = log4js.getLogger('pkg-pick-list-component-spec')
-logger.level = 'debug'
+logger.level = 'warn'
 
 describe('PkgPickList', () => {
   describe('construction', () => {
@@ -37,17 +39,18 @@ describe('PkgPickList', () => {
 
   describe('render', () => {
     it('renders with empty lists', () => {
-      const dom = new PkgPickList().render()
+      const fred = new PkgPickList()
+      const dom = fred.render()
       const expected = <div className='package-pick-list'>
-        <PkgListComponent className='pick-list-left-list' items={new Immutable.Set([])} />
+        <PkgSelectList className='pick-list-left-list' on={{PkgSelectListChangeEvent: fred.handleLeftSelectListChange}} items={new Immutable.Set([])} />
         <div className='pick-list-button-col'>
-          <button className='pick-list-btn-move-right'>&lt;&lt;</button>
-          <button className='pick-list-btn-move-left'>&gt;&gt;</button>
+          <button onclick={fred.handleLeftButton} className='pick-list-btn-move-right'>&lt;&lt;</button>
+          <button onclick={fred.handleRightButton} className='pick-list-btn-move-left'>&gt;&gt;</button>
         </div>
-        <PkgListComponent className='pick-list-right-list' items={new Immutable.Set([])} />
+        <PkgSelectList className='pick-list-right-list' on={{PkgSelectListChangeEvent: fred.handleRightSelectListChange}} items={new Immutable.Set([])} />
       </div>
-
-      // logger.debug((new DomDiff(dom, expected)).toString())
+      const diff = new DomDiff(dom, expected)
+      logger.debug(diff.toString())
       expect(dom).toEqual(expected)
     })
 
@@ -61,12 +64,12 @@ describe('PkgPickList', () => {
       const dom = pick.render()
       expect(dom).toEqual(
         <div className='package-pick-list'>
-          <PkgListComponent className='pick-list-left-list' items={new Immutable.Set(['pkg4', 'pkg5'])} />
+          <PkgSelectList className='pick-list-left-list' on={{PkgSelectListChangeEvent: pick.handleLeftSelectListChange}} items={new Immutable.Set(['pkg4', 'pkg5'])} />
           <div className='pick-list-button-col'>
-            <button className='pick-list-btn-move-right'>&lt;&lt;</button>
-            <button className='pick-list-btn-move-left'>&gt;&gt;</button>
+            <button onclick={pick.handleLeftButton} className='pick-list-btn-move-right'>&lt;&lt;</button>
+            <button onclick={pick.handleRightButton} className='pick-list-btn-move-left'>&gt;&gt;</button>
           </div>
-          <PkgListComponent className='pick-list-right-list' items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3'])} />
+          <PkgSelectList className='pick-list-right-list' on={{PkgSelectListChangeEvent: pick.handleRightSelectListChange}} items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3'])} />
         </div>
       )
     })
@@ -85,12 +88,35 @@ describe('PkgPickList', () => {
       const dom = pick.render()
       expect(dom).toEqual(
         <div className='package-pick-list' id='my-pick-list' kumquat='fruitbat'>
-          <PkgListComponent className='pick-list-left-list' items={new Immutable.Set(['pkg4', 'pkg5'])} />
+          <PkgSelectList className='pick-list-left-list' on={{PkgSelectListChangeEvent: pick.handleLeftSelectListChange}} items={new Immutable.Set(['pkg4', 'pkg5'])} />
           <div className='pick-list-button-col'>
-            <button className='pick-list-btn-move-right'>&lt;&lt;</button>
-            <button className='pick-list-btn-move-left'>&gt;&gt;</button>
+            <button onclick={pick.handleLeftButton} className='pick-list-btn-move-right'>&lt;&lt;</button>
+            <button onclick={pick.handleRightButton} className='pick-list-btn-move-left'>&gt;&gt;</button>
           </div>
-          <PkgListComponent className='pick-list-right-list' items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3'])} />
+          <PkgSelectList className='pick-list-right-list' on={{PkgSelectListChangeEvent: pick.handleRightSelectListChange}} items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3'])} />
+        </div>
+      )
+    })
+
+    it('appends other classes to top-level div', () => {
+      const pick = new PkgPickList({
+        rightList: [
+          'pkg1', 'pkg2', 'pkg3'
+        ],
+        leftList: [
+          'pkg4', 'pkg5'
+        ],
+        className: 'some-class some-other-class'
+      })
+      const dom = pick.render()
+      expect(dom).toEqual(
+        <div className='package-pick-list some-class some-other-class'>
+          <PkgSelectList className='pick-list-left-list' on={{PkgSelectListChangeEvent: pick.handleLeftSelectListChange}} items={new Immutable.Set(['pkg4', 'pkg5'])} />
+          <div className='pick-list-button-col'>
+            <button onclick={pick.handleLeftButton} className='pick-list-btn-move-right'>&lt;&lt;</button>
+            <button onclick={pick.handleRightButton} className='pick-list-btn-move-left'>&gt;&gt;</button>
+          </div>
+          <PkgSelectList className='pick-list-right-list' on={{PkgSelectListChangeEvent: pick.handleRightSelectListChange}} items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3'])} />
         </div>
       )
     })
@@ -110,18 +136,16 @@ describe('PkgPickList', () => {
         let dom = pick.render()
         expect(dom.children[0].props['items'].toJS()).toEqual(['pkg4'])
         expect(dom.children[2].props['items'].toJS()).toEqual(['pkg1', 'pkg2', 'pkg3', 'pkg5'])
-        /* The following does not compare Immutable.Set values for "items" as equal, even though it works on
-         * other tests. why? */
-        // expect(dom).toEqual(
-        //   <div className='package-pick-list'>
-        //     <PkgListComponent className='pick-list-left-list' items={new Immutable.Set(['pkg4'])} />
-        //     <div className='pick-list-button-col'>
-        //       <button className='pick-list-btn-move-right'>&lt;&lt;</button>
-        //       <button className='pick-list-btn-move-left'>&gt;&gt;</button>
-        //     </div>
-        //     <PkgListComponent className='pick-list-right-list' items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3', 'pkg5'])} />
-        //   </div>
-        // )
+        expect(dom).toEqual(
+          <div className='package-pick-list'>
+            <PkgSelectList className='pick-list-left-list' on={{PkgSelectListChangeEvent: pick.handleLeftSelectListChange}} items={new Immutable.Set(['pkg4'])} />
+            <div className='pick-list-button-col'>
+              <button className='pick-list-btn-move-right'>&lt;&lt;</button>
+              <button className='pick-list-btn-move-left'>&gt;&gt;</button>
+            </div>
+            <PkgSelectList className='pick-list-right-list' on={{PkgSelectListChangeEvent: pick.handleRightSelectListChange}} items={new Immutable.Set(['pkg1', 'pkg2', 'pkg3', 'pkg5'])} />
+          </div>
+        )
       })
     })
 
@@ -138,7 +162,32 @@ describe('PkgPickList', () => {
         let dom = pick.render()
         expect(dom.children[0].props['items'].toJS()).toEqual(['pkg4', 'pkg5', 'pkg3'])
         expect(dom.children[2].props['items'].toJS()).toEqual(['pkg1', 'pkg2'])
+        expect(dom).toEqual(
+          <div className='package-pick-list'>
+            <PkgSelectList className='pick-list-left-list' items={new Immutable.Set(['pkg4'], 'pkg5', 'pkg3')} />
+            <div className='pick-list-button-col'>
+              <button className='pick-list-btn-move-right'>&lt;&lt;</button>
+              <button className='pick-list-btn-move-left'>&gt;&gt;</button>
+            </div>
+            <PkgSelectList className='pick-list-right-list' items={new Immutable.Set(['pkg1', 'pkg2'])} />
+          </div>
+        )
       })
+    })
+  })
+
+  describe('event handling', () => {
+    it('raises PickListMoveButtonEvent', () => {
+      const pick = new PkgPickList({
+        rightList: [
+          'pkg1', 'pkg2', 'pkg3'
+        ],
+        leftList: ['pkg4', 'pkg5']
+      })
+      pick.move('right', 'pkg4')
+    })
+    it('responds to PkgSelectListChangeEvent', () => {
+
     })
   })
 })
