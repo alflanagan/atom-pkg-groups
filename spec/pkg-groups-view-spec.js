7b6ce93179e8993e1b2e49498186874e53bc1cde
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-02-05 17:07:23 -0500

    revisit assumptions and rewrite -- breaking!

--- spec/pkg-groups-view-spec.js
+++ spec/pkg-groups-view-spec.js
@@ -9,35 +9,31 @@ import PkgSelectList from '../lib/pkg-select-list-component'
 import PkgPickList from '../lib/pkg-pick-list-component'
 import PkgTristateList from '../lib/pkg-tristate-list-component'
 import PkgGroupsView from '../lib/pkg-groups-view'
+import PkgGroupsModel from '../lib/pkg-groups-model'
+import PkgGroupsGroup from '../lib/pkg-groups-group'
+import PkgGroupsMeta from '../lib/pkg-groups-meta'
 
 const logger = log4js.getLogger('pkg-groups-view-spec')
-logger.level = 'info'
+logger.level = 'debug'
 
 describe('PkgGroupsView', () => {
   describe('constructor', () => {
-    it('does not require arguments', () => {
-      let fred = new PkgGroupsView()
-      expect(fred.children).not.toExist()
-      expect(fred.props).toEqual({available: [], selected: [], groups: [], metas: [], on: {change: null, select: null}})
-      // attributes added to component by etch.initialize()
-      expect(fred.virtualNode['tag']).toEqual('div')
-      expect(fred.virtualNode['props']['className']).toEqual('pkg-groups')
-      expect(fred.virtualNode['children']).toBeInstanceOf(Array)
-      expect(fred.virtualNode.children[0].props).toEqual({id: 'group-select-panel'})
-      expect(fred.element).toExist()
-      expect(fred.refs).toEqual({})
-    })
-
     it('sets properties correctly', () => {
       const tavailable = ['package1', 'package2', 'package3']
       const tselected = ['package4', 'package5']
       const tgroups = ['Tar Heels', 'Wolfpack', 'Wildcats']
+      const groups = []
+      for (const grp of tgroups) {
+        groups.push(new PkgGroupsGroup({name: grp, type: 'group', packages: tselected}))
+      }
       const tmetas = {baseball: 'enabled', basketball: 'disabled'}
+      const metas = []
+      for (const name in tmetas) {
+        metas.push(new PkgGroupsMeta({name}))
+      }
+      const model = new PkgGroupsModel({groups, metas})
       const pkgView = new PkgGroupsView({
-        available: tavailable,
-        selected: tselected,
-        groups: tgroups,
-        metas: tmetas
+        model: model
       })
       expect(pkgView.props.available).toEqual(tavailable)
       expect(pkgView.props.selected).toEqual(tselected)
@@ -85,6 +81,7 @@ describe('PkgGroupsView', () => {
         logger.warn(diff.toString())
       }
       expect(diff.noDifferences()).toBe(true)
+      logger.debug(view.getElement())
     })
 
     it('renders properties as expected', () => {
@@ -153,11 +150,11 @@ describe('PkgGroupsView', () => {
       const parent = document.createElement('div')
       parent.appendChild(etch.render(pkgView))
       logger.debug(pkgView.element.querySelector('#groups-select-list'))
-      const leftElem = pkgView.element.querySelector('#groups-select-list>ol').children[1]
-      expect(leftElem).toBeInstanceOf(global.HTMLLIElement)
-      expect(leftElem.innerText).toEqual('Wolfpack')
-      const index = global.parseInt(leftElem.attributes.item(0).value)
-      leftElem.click()
+      const groupElem = pkgView.element.querySelector('#groups-select-list>ol').children[1]
+      expect(groupElem).toBeInstanceOf(global.HTMLLIElement)
+      expect(groupElem.innerText).toEqual('Wolfpack')
+      const index = global.parseInt(groupElem.attributes.item(0).value)
+      groupElem.click()
       // this should select a group and display its packages. Since we don't have a model,
       // the only thing that happens is the callback is called
       const actual = pkgView.render()
@@ -196,11 +193,76 @@ describe('PkgGroupsView', () => {
       }
       expect(actual).toEqual(expected)
       expect(mySpy).toHaveBeenCalled()
-      expect(mySpy.calls[0].args).toEqual([leftElem.innerText, index])
+      expect(mySpy.calls[0].args).toEqual([groupElem.innerText, index])
     })
 
     it('responds to clicks on available list', () => {
-
+      logger.debug('test click on package start')
+      const mySpy = jasmine.createSpy((item, index) => {})
+      const tavailable = ['package1', 'package2', 'package3']
+      const tselected = ['package4', 'package5']
+      const tgroups = ['Tar Heels', 'Wolfpack', 'Wildcats']
+      const tmetas = {'baseball': 'disabled', 'basketball': 'enabled'}
+      const pkgView = new PkgGroupsView({
+        available: tavailable,
+        selected: tselected,
+        groups: tgroups,
+        metas: tmetas,
+        on: {change: mySpy}
+      })
+      const parent = document.createElement('div')
+      parent.appendChild(etch.render(pkgView))
+      logger.debug(pkgView.element.querySelector('#pkg-groups-group-pick'))
+      const pkg2Elem = pkgView.element.querySelector('#pkg-groups-group-pick .pick-list-left-list .pkg-select-list>ol').children[1]
+      expect(pkg2Elem).toBeInstanceOf(global.HTMLLIElement)
+      expect(pkg2Elem.innerText).toEqual('package2')
+      const index = global.parseInt(pkg2Elem.attributes.item(0).value)
+      pkg2Elem.click()
+      logger.debug('after click()')
+      expect(pkgView.props.available).toEqual(['package1', 'package3'])
+      const actual = pkgView.render()
+      logger.debug('after render()')
+      const expected = <div className='pkg-groups'>
+        <atom-panel id='group-select-panel'>
+          <div id='pkg-groups-upper-panel' className='inset-panel padded'>
+            <h1>Set Up Package Groups</h1>
+            <PkgSelectList id='groups-select-list' items={tgroups} on={{change: pkgView._didSelectGroup}} />
+            <button id='groups-add-group' name='add-group' type='button' ><i class='fa fa-plus-circle' aria-hidden='true' /></button>
+            <button id='groups-delete-group' name='delete-group' type='button' ><i class='fa fa-minus-circle' aria-hidden='true' /></button>
+            <PkgPickList leftList={tavailable} rightList={tselected} id='pkg-groups-group-pick'
+              leftLabel='Packages Available' rightLabel='Packages In Group' on={{change: pkgView._didChange}} />
+            <p className='group-select-hint'>Click Package Name to Select/Deselect</p>
+          </div>
+          <div id='pkg-groups-lower-panel' className='inset-panel padded'>
+            <div id='defined-metas-panel'>
+              <h1>Set Up Configurations</h1>
+              <div id='meta-select-list'>
+                <PkgSelectList items={tmetas} on={{change: pkgView._didSelectMeta}} />
+                <button id='meta-add-meta' name='add-meta' type='button' ><i class='fa fa-plus-circle' aria-hidden='true' /></button>
+                <button id='meta-delete-meta' name='delete-meta' type='button' ><i class='fa fa-minus-circle' aria-hidden='true' /></button>
+              </div>
+            </div>
+            <div id='specify-metas-panel'>
+              <h2>Activate / Deactivate Groups</h2>
+              <div className='sub-section group-selection'>
+                <PkgTristateList items={tmetas} on={{change: pkgView._didChangeMeta}} />
+              </div>
+            </div>
+          </div>
+        </atom-panel>
+      </div>
+      logger.debug('created expected')
+      const diff = new DomDiff(actual, expected)
+      if (!diff.noDifferences()) {
+        logger.warn(diff.toString())
+      }
+      expect(actual).toEqual(expected)
+      expect(mySpy).toHaveBeenCalled()
+      logger.debug('after toHaveBeenCalled()')
+      if (mySpy.calls[0].args) {
+        expect(mySpy.calls[0].args).toEqual([pkg2Elem.innerText, index])
+      }
+      logger.debug('test click on package end')
     })
   })
 })
